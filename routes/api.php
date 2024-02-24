4c173000c32670c5dd46f20637e2bb1fd9177c3e
Kristina Naumova <kristinatomsk29@gmail.com>
2024-02-25 02:31:26 +0700

    Получение всех локаций

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,12 @@ Route::prefix('/user')->controller('App\Http\Controllers\UserController')->group
     });
 });
 
+Route::prefix('/location')->controller('App\Http\Controllers\LocationController')->group(function () {
+    Route::middleware('auth.jwt')->group(function () {
+       Route::get('', 'getAllLocations');
+    });
+});
+
 Route::fallback(function () {
     return response(['message' => 'Undefined route'], 404);
 });
