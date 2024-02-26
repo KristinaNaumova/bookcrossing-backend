bf8b8be961042a49f5c7f2be63d54fee434a32ad
Kristina Naumova <kristinatomsk29@gmail.com>
2024-02-26 19:17:08 +0700

    Работа сервиса контроля сделок по расписанию

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Services\DealControllerService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            (new DealControllerService())->checkWaitingDeals();
+        })->everyMinute();
+
+        $schedule->call(function () {
+            (new DealControllerService())->checkDealsInProcess();
+        })->everyTwoHours();
     }
 
     /**
---
+++ app/Services/DealControllerService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Ad;
+use App\Models\Deal;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class DealControllerService
+{
+    public function checkWaitingDeals()
+    {
+        $deals = Deal::where('deal_status', 'DealWaiting')
+            ->where('deal_waiting_end_time', '<=', date('Y-m-d H:i'))
+            ->get();
+
+        foreach ($deals as $deal) {
+            $dealId = $deal['id'];
+
+            $dealFromDB = Deal::find($dealId);
+
+            DB::transaction(function () use ($dealFromDB, $dealId) {
+                $ad = Ad::find($dealId);
+
+                $ad->update([
+                    'status' => 'Active',
+                ]);
+
+                $dealFromDB->delete();
+            });
+        }
+    }
+
+    public function checkDealsInProcess()
+    {
+        $deals = Deal::where('deal_status', 'RefundWaiting')
+            ->where('refund_waiting_end_time', '<=', date('Y-m-d H:i'))
+            ->get();
+
+        foreach ($deals as $deal) {
+            $userId = $deal['second_member_id'];
+
+            $user = User::find($userId);
+            $userNewRating = number_format((float)(($user['rating'] ?? 0) - 0.01), 2, '.', '');
+
+            $user->update([
+                'rating' => $userNewRating,
+            ]);
+        }
+    }
+}
